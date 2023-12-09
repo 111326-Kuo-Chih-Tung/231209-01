e6f87ba80c6426839126aeb9e9cbe27c03b18225
111326-Kuo-Chih-Tung <153349297+111326-Kuo-Chih-Tung@users.noreply.github.com>
2023-12-09 14:44:13 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+for (let 次序 = 0; 次序 <= 4; 次序++) {
+    if (次序 % 2 == 0) {
+        for (let Y = 0; Y <= 4; Y++) {
+        	
+        }
+    } else {
+        for (let X = 0; X <= 4; X++) {
+            led.plot(0, 0)
+        }
+    }
+}
+basic.forever(function () {
+	
+})
